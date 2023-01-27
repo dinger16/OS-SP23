fe0d4da152713d6ef68259c49d5606958888fcc7
jacklambert1 <jlamber4@nd.edu>
2023-01-26 20:35:31 -0500

    more modular bytecheck.c and adding Makefile

--- Milestone1/bytecheck.c
+++ Milestone1/bytecheck.c
@@ -4,36 +4,38 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int main(int argc, char* argv[]) {
+void check_num_args(int argc) {
+    // there should be three arguments: the program name, the filename, and the lookup byte
     if (argc != 3) {
         fprintf(stderr, "Error: Incorrect number of inputs!\n");
         exit(1);
     }
-    char* filename;
-    char* end;
-    long int lookup_byte;
+}
 
-    // Check if hex value starts with '0x'
-    if (argv[2][0] != '0' || argv[2][1] != 'x') {
+void check_hex(char* hex) {
+    // the lookup hex byte should be in the form '0x##'
+    if (hex[0] != '0' || hex[1] != 'x') {
         fprintf(stderr, "Error: Invalid lookup byte!\n");
         exit(1);
     }
+}
 
-    
-    lookup_byte = strtol(argv[2], &end, 16);
+void check_lookup_byte(long int lookup_byte, char* end) {
+    // the lookup byte should be a valid hex digit
     if (*end != '\0') {
         fprintf(stderr, "Error: Invalid lookup byte!\n");
         exit(1);
     }
-
-
+    // the lookup byte should be less than 0x99
     if (lookup_byte > 0x99) {
         fprintf(stderr, "Error: Hex value too large!\n");
         exit(1);
     }
+}
 
-    filename = strdup(argv[1]);
+struct stat check_file_stats(char* filename) {
     struct stat sb;
+    // the file should exist, be a regular file, and be less than 25kB
     if (stat(filename, &sb) != 0) {
         fprintf(stderr, "Error: File does not exist!\n");
         exit(1);
@@ -48,20 +50,26 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    /* Fix permission issue later */
+    return sb;
+}
 
-    // if (!(sb.st_mode & S_IRUSR)) {
-    //     fprintf(stderr, "You do not have permission to read this file!\n");
-    //     exit(1);
-    // }
+void check_readability(char* filename) {
+    // the file should be readable
+    if (access(filename, R_OK) != 0) {
+        fprintf(stderr, "Error: Unable to open! File was not readable.\n");
+        exit(1);
+    }
+}
 
-    FILE* fp = fopen(filename, "r");
-    char buffer[sb.st_size];
-    size_t result = fread(buffer, sizeof(char), sb.st_size, fp);
+void check_size(struct stat sb, int result) {
+    // if read correctly, the number of bytes read should equal the file size
     if (result != sb.st_size) {
         fprintf(stderr, "Error: Reading error");
     }
+}
 
+void count_lookup_byte(char* buffer, struct stat sb, long int lookup_byte) {
+    // iterate through all the bytes in the buffer and count the number of itmes the lookup byte appears
     int i, count = 0;
     for (i = 0; i < sb.st_size; i++) {
         if (buffer[i] == lookup_byte) {
@@ -69,6 +77,31 @@ int main(int argc, char* argv[]) {
         }
     }
     printf("%d\n", count);
+}
+
+int main(int argc, char* argv[]) {
+
+    char* filename;
+    char* end;
+    long int lookup_byte;
+
+    // Check the user inputter the correct number of arguments, a valid hex digit as a lookup byte, and a valid file
+    check_num_args(argc);
+    check_hex(argv[2]);
+    lookup_byte = strtol(argv[2], &end, 16);
+    check_lookup_byte(lookup_byte, end);
+    filename = strdup(argv[1]);
+    struct stat sb = check_file_stats(filename);
+
+    // Check the file is readable and if so then open it, read it into a buffer, and check the file was read correctly
+    check_readability(filename);
+    FILE* fp = fopen(filename, "r");
+    char buffer[sb.st_size];
+    size_t result = fread(buffer, sizeof(char), sb.st_size, fp);
+    check_size(sb, result);
+
+    // Count the number of times the lookup byte appears in the file and print it
+    count_lookup_byte(buffer, sb, lookup_byte);
 
     return 0;
 }
