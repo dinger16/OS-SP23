70bec69a18d2d0fc354755b43eead20a73fcdafe
jacklambert1 <jlamber4@nd.edu>
2023-02-02 16:38:55 -0500

    More rigorous error checking

--- Project01/byteflip.c
+++ Project01/byteflip.c
@@ -4,10 +4,11 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int nMaxFileSize = 25000;
-char* userRequest = "bf";
+long int nMaxFileSize = 25000;
+char* userRequest = "-bf";
 char* out_filename = NULL;
-bool given_out_filename = false;
+int given_out_filename = 0;
+char* in_filename = NULL;
 
 void check_num_args(int argc) {
     // there should be three arguments: the program name, the filename, and the lookup byte
@@ -19,9 +20,11 @@ void check_num_args(int argc) {
 
 void process_args(int argc, char* argv[]) {
 
-    int i = 2;
+    int i = 1;
     int request_count = 0;
     int maxsize_count = 0;
+    int out_filename_count = 0;
+    int in_filename_count = 0;
 
     while (i < argc) {
         
@@ -45,10 +48,12 @@ void process_args(int argc, char* argv[]) {
         } else if (strcmp(argv[i], "-maxsize") == 0) {
             
             i++;
-
+            if (i == argc) {
+                fprintf(stderr, "Error: Too few inputs!\n");
+                exit(1);
+            }
             char* endptr = NULL;
             nMaxFileSize = strtol(argv[i], &endptr, 10);
-
             // check if the user entered a valid integer
             if (*endptr != '\0') {
                 fprintf(stderr, "Error: Invalid maxsize argument!\n");
@@ -56,6 +61,9 @@ void process_args(int argc, char* argv[]) {
             }
             if (nMaxFileSize < 0) {
                 fprintf(stderr, "Error: Invalid maxsize argument!\n");
+                exit(1);
+            } else if (nMaxFileSize > 100000000) {
+                fprintf(stderr, "Error: Maxsize exceeds 100,000,000 bytes!\n");
                 exit(1);
             }
 
@@ -69,8 +77,38 @@ void process_args(int argc, char* argv[]) {
 
         } else if (strcmp(argv[i], "-o") == 0) {
             i++;
+
+            if (i == argc) {
+                fprintf(stderr, "Error: Too few inputs!\n");
+                exit(1);
+            }
+
             out_filename = strdup(argv[i]);
-            given_out_filename = true;
+            given_out_filename = 1;
+
+            out_filename_count++;
+            
+            if (out_filename_count > 1) {
+                fprintf(stderr, "Error: Too many output filename arguments!\n");
+                exit(1);
+            }
+
+        } else if (argv[i][0] != '-') {
+            in_filename = strdup(argv[i]);
+            in_filename_count++;
+
+            if (in_filename_count > 1) {
+                fprintf(stderr, "Error: Too many input filename arguments!\n");
+                exit(1);
+            }
+        } else {
+            fprintf(stderr, "Potential Arguments:\n");
+            fprintf(stderr, "\t-maxsize XXX: Set the maximum file size to XXX bytes.\n");
+            fprintf(stderr, "\t-o XXX: Set the output file name to XXX.\n");
+            fprintf(stderr, "\t-r: Return ther reverse order of bytes.\n");
+            fprintf(stderr, "\t-bf: Return the bit-flipped order of bytes.\n");
+            fprintf(stderr, "\t-help: Print this help message.\n");
+            exit(1);
         }
 
         i ++;
@@ -79,7 +117,7 @@ void process_args(int argc, char* argv[]) {
 
 }
 
-struct stat check_file_stats(char* filename, int nMaxFileSize) {
+struct stat check_file_stats(char* filename) {
     struct stat sb;
     // the file should exist, be a regular file, and be less than 25kB
     if (stat(filename, &sb) != 0) {
@@ -92,7 +130,7 @@ struct stat check_file_stats(char* filename, int nMaxFileSize) {
     }
 
     if (sb.st_size > nMaxFileSize) {
-        fprintf(stderr, "Error: The file is over 25kB (file size was %lld bytes)\n", (long long) sb.st_size);
+        fprintf(stderr, "Error: The file is over %ld bytes (file size was %lld bytes)\n", nMaxFileSize, (long long) sb.st_size);
         exit(1);
     }
 
@@ -134,8 +172,17 @@ void reverse(char* buffer, int length) {
 }
 
 void write_to_file(char* buffer, int length, char* filename) {
+
+    struct stat sb;
+    // the file should not exist
+    if (stat(filename, &sb) == 0) {
+        fprintf(stderr, "Error: Output file already exist!\n");
+        exit(1);
+    }
+
     // write buffer to the output file
     FILE* fp = fopen(filename, "w");
+
     fwrite(buffer, sizeof(char), length, fp);
     fclose(fp);
 }
@@ -146,48 +193,49 @@ int main(int argc, char* argv[]) {
     process_args(argc, argv); // process the user's input
 
     // initialize input and ouput file names
-    char* in_filename;
+    //char* in_filename;
     
-    in_filename = strdup(argv[1]);
-    struct stat sb = check_file_stats(in_filename, nMaxFileSize); // checks for object's existence, type, and size
+    //in_filename = strdup(argv[1]);
+    if (!in_filename) {
+        fprintf(stderr, "Error: No input file specified!\n");
+        exit(1);
+    }
+    
+    struct stat sb = check_file_stats(in_filename); // checks for object's existence, type, and size
     check_readability(in_filename); // checks for read permissions
 
     FILE* fp = fopen(in_filename, "r");
-    char buffer[sb.st_size]; // allocate buffer according to filesize
+    char* buffer = (char*) malloc(sb.st_size * sizeof(char)); // allocate buffer according to filesize
     size_t result = fread(buffer, sizeof(char), sb.st_size, fp);
     check_size(sb, result); // check the read was successful
 
-    // reverse and/or bitflip the buffer based on the user's input
-    if (strcmp(userRequest, "bf") == 0) {
+    // reverse and/or bitflip the buffer based on the user's input and assign appropriate extension
+    char* extension;
+    if (strcmp(userRequest, "-bf") == 0) {
         bitflip(buffer, sb.st_size);
-    } else if (strcmp(userRequest, "r") == 0) {
+        extension = ".bf";
+    } else if (strcmp(userRequest, "-r") == 0) {
         reverse(buffer, sb.st_size);
+        extension = ".r";
     } else {
         bitflip(buffer, sb.st_size);
         reverse(buffer, sb.st_size);
-    }
-
-    // assign the appropriate extension to the output file based on the user input
-    char* extension;
-    if (strcmp(userRequest, "bf") == 0) {
-        extension = ".bf";
-    }
-    else if (strcmp(userRequest, "r") == 0) {
-        extension = ".r";
-    }
-    else  {
         extension = ".bfr";
     }
 
     // allocate memory for ouput file and add the appropriate extension
-    if (!given_out_filename) {
+    if (given_out_filename == 0) {
         out_filename = strdup(in_filename);
+        out_filename = strcat(out_filename, extension);
     }
-    out_filename = strcat(out_filename, extension);
 
     write_to_file(buffer, sb.st_size, out_filename); // write the buffer to the output file
 
     fclose(fp); // close the input file
+    free(buffer); // free the buffer
+
+    fprintf(stdout, "Input: %s was %d bytes\n", in_filename, (int) sb.st_size);
+    fprintf(stdout, "Output: %s was output successfully\n", out_filename);
 
     return 0;
 }
@@ -195,6 +243,5 @@ int main(int argc, char* argv[]) {
 
 /*
 CURRENT THINGS TO ADDRESS:
-    - currently assumes filename is always first argument following executable
-    - should we put a limit on the max size a user specifies?
+    - what to do if output file already exists
 */
